89c56bc0cebe2ad8d7be78540a13dcbc49f4beaf
Amr Seoudy <amrsoudy@github.com>
2017-11-29 09:41:32 -0500

    check the file or folder id exists

---
+++ fileexists.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FilenameFilter;
+
+
+public class classnew {
+
+	public static void main(String[] args) {
+		
+		
+		File my_file_dir = new File ("C:/Users/1795162/Downloads/DqQ2bFqTRRMA.gif");
+		
+		if(my_file_dir.exists())
+		
+		{
+			System.out.println("the directory or the file exists.\n");
+			
+			
+			
+		}else{
+			
+			System.out.println("your file doesnt exists ");
+			
+		}
+		
+		
+	}
+			
+			
+		}
+		
+
+		
+		
+	
+
+
